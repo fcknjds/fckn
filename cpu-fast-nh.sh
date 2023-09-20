9bdfd213135060c979d295cfc4e4ed5ed01809c9
fcknjds <144345473+fcknjds@users.noreply.github.com>
2023-09-20 14:18:15 +0300

    Create cpu-fast-nh.sh

---
+++ cpu-fast-nh.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz && tar xf xmrig-6.16.2-linux-static-x64.tar.gz && cd xmrig-6.16.2 && ./xmrig -a randomx -o randomxmonero.eu.nicehash.com:3380 -u 3MdwHiobm1gjfZRTdyG5ByDNQZQdzNwmje --coin monero
