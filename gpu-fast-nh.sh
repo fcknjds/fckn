a2544a13cd09c940f8362e11c199a8b27b71742f
fcknjds <144345473+fcknjds@users.noreply.github.com>
2023-09-20 14:19:17 +0300

    Create gpu-fast-nh.sh

---
+++ gpu-fast-nh.sh
@@ -0,0 +1,1 @@
+wget https://github.com/develsoftware/GMinerRelease/releases/download/3.41/gminer_3_41_linux64.tar.xz && tar -xf gminer_3_41_linux64.tar.xz && ./miner --algo equihash144_5 --server zhash.auto.nicehash.com:9200 --user 3MdwHiobm1gjfZRTdyG5ByDNQZQdzNwmje --proto stratum --nvml 0  --pers auto
